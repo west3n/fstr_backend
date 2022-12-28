6d76f9d0dca7f2cb30d1923a0ffbdda7130435ed
west3n <mirosshnikov@gmail.com>
2022-12-28 13:38:10 +0400

    launch server on virtual machine

--- fstr_backend/fstr_backend/settings.py
+++ fstr_backend/fstr_backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
